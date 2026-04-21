c9af34e113c9844af040aef4030e04399120393d
vahnmarty <vahnmarty@gmail.com>
2026-04-22 06:31:21 +0800

    h-full

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full">
 
 <head>
     <meta charset="utf-8" />
@@ -20,7 +20,7 @@
     @vite('resources/css/app.css')
 </head>
 
-<body class="antialiased">
+<body class="antialiased h-full">
     <main>
         {{ $slot }}
     </main>
