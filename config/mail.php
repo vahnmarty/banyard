53e49de4e2eb99f6385f4f667410347bc6226512
vahnmarty <vahnmarty@gmail.com>
2026-04-27 18:13:35 +0800

    resend

--- config/mail.php
+++ config/mail.php
@@ -37,6 +37,10 @@ return [
 
     'mailers' => [
 
+        'resend' => [
+            'transport' => 'resend',
+        ],
+
         'smtp' => [
             'transport' => 'smtp',
             'scheme' => env('MAIL_SCHEME'),
